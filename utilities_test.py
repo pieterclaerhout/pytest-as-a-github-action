b0b68eaa496f82d8f33724213066db7048630ec2
Pieter Claerhout <pieter@yellowduck.be>
2020-10-24 10:51:15 +0200

    Added a failing test

--- utilities_test.py
+++ utilities_test.py
@@ -1,7 +1,12 @@
 import utilities
 
+# Sample of a working test
 def test_uuid():
     uuid = utilities.create_uuid()
     assert len(uuid) == 32
     assert uuid.find('-') == -1
  
+# Sample of a failing test
+def test_failing():
+    uuid = utilities.create_uuid()
+    assert uuid is None
